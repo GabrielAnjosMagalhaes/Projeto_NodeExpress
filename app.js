dad9822b80d2e8149a11b45497f56ecc0a55f931
Gabriel dos Anjos <95793375+GabrielAnjosMagalhaes@users.noreply.github.com>
2022-07-06 08:59:19 -0300

    Primeiras mudanças

--- app.js
+++ app.js
@@ -1,9 +1,14 @@
 const express = require("express")
 const port = 3000
 const app = express()
+const path = require ("path")
 
-app.get('/', (req, res, next) => {
-    res.send({
+//setup view engine
+app.set(`views`, path.join(__dirname, "views"))
+app.set('view engine', 'ejs')
+
+app.get('/', function(req, res, next) {
+    res.render('index', {
         title: "Express trabalho DevWeb",
         version: "0.0.0"
     })
@@ -12,3 +17,5 @@ app.get('/', (req, res, next) => {
 app.listen(port, err => {
     console.log(`Server is listening on ${port}`)
 })
+
+app.get('/')

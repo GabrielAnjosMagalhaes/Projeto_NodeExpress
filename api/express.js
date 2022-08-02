e9f3ffecced2896740f8a69479cdbd8a62022f3b
Gabriel dos Anjos <95793375+GabrielAnjosMagalhaes@users.noreply.github.com>
2022-08-02 11:21:36 -0300

    Criando página, mostrar

--- api/express.js
+++ api/express.js
@@ -2,22 +2,29 @@ const express = require('express')
 const app = express();
 const port = 3000
 
-app.listen(port, () => console.log(`Estamos rodando na port ${port}`))
+app.set("view engine", "ejs")
+app.use(express.static('./styles'))
+
 
 app.get('/home', (req, res) => {
     res.status(200).send("<h1>Essa é nossa página inicial</h1>")
+    
 })
 
 app.get('/nossaapi', (req, res) => {
 const criandoApi = [
     {
-        informação1: "alguma coisa 1",
-        informação2: "alguma coisa",
+        info1: "alguma coisa 1",
+        info2: "alguma coisa",
     },
     {
-        informação1: "alguma coisa 3",
-        informação2: "alguma coisa"
+        info1: "alguma coisa 3",
+        info2: "alguma coisa"
     }
 ]
-res.status(200).json(criandoApi)
+res.status(200).render("index", {info: criandoApi})
 })
+
+
+
+app.listen(port, () => console.log(`Estamos rodando na port ${port}`))

ee644e1cf201aa8ed3f78d5b28844d47a3dec521
Gabriel dos Anjos <95793375+GabrielAnjosMagalhaes@users.noreply.github.com>
2022-08-03 08:17:04 -0300

    Adicionando ditados

--- api/express.js
+++ api/express.js
@@ -12,19 +12,37 @@ app.get('/home', (req, res) => {
 })
 
 app.get('/nossaapi', (req, res) => {
-const criandoApi = [
+const ditadosAPI = [
     {
-        info1: "alguma coisa 1",
-        info2: "alguma coisa",
+        prov1: "Conselho 1: Cobra que não anda, não engole sapo."       
     },
     {
-        info1: "alguma coisa 3",
-        info2: "alguma coisa"
+        prov1: "Conselho 2: Muito ajuda quem não atrapalha."       
+    },
+    {
+        prov1: "Conselho 3: Não há que ser forte. Há que ser flexível."       
+    },
+    {
+        prov1: "Conselho 4: Macaco velho não mete a mão em cumbuca."       
+    },
+    {
+        prov1: "Conselho 5: A palavra é prata, o silêncio é ouro."       
+    },
+    {
+        prov1: "Conselho 6: Passarinho que acompanha morcego acorda de cabeça para baixo."       
+    },
+    {
+        prov1: "Conselho 7: Não há bem que sempre dure, nem mal que nunca se acabe."       
+    },
+    {
+        prov1: "Conselho 8: Se queres a paz, prepara-te para a guerra."       
     }
 ]
+
+
 // para buscar informações: res.status(200).render|("nomedoarquivo)"|,|({info: informacaoQueVoceQuerPassar})| 
 //nesse caso usei o index vindo da pasta views
-res.status(200).render("index", {info: criandoApi})
+res.status(200).render("index", {info: ditadosAPI})
 })
 
 
